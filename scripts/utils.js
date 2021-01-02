b0926c314a477ebbdfcff35722d4f0c5dad0141a
s_stivason <stivason@gmail.com>
2021-01-02 12:30:47 -0500

    Project 7 Initial Commit

--- scripts/utils.js
+++ scripts/utils.js
@@ -14,13 +14,16 @@ export function closeModalOutside(event) {
   togglePopup(event.target);
 } 
  
-
 export function escCloseModal(event) { 
   if (event.key === 'Escape') { 
     const openModal = document.querySelector('.popup_opened'); 
     togglePopup(openModal); 
   } 
 }
+
+/* NOTE TO REVIEWER: The ESC works great until I save one of the forms, 
+then ESC stops working for the Image modals only.  Could you nudge me in 
+the right direction to address this? */
 
 export function saveButtonDisabled(modal) {
   const inactiveButton = modal.querySelector('.popup__save-button');
